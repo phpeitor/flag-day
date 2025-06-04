5b9b450ec4f07d538b32ae699c08ea6756661ab6
FORTELalejandro.montalvan <alejandro.montalvan@fortelcorp.com>
2025-06-04 12:19:43 -0500

    release commit

--- js/script.js
+++ js/script.js
@@ -1,56 +1,40 @@
 const sparkle = document.querySelector(".sparkle");
-
 var current_star_count = 0;
-
 const MAX_STARS = 30;
 const STAR_INTERVAL = 100;
-
 const MAX_STAR_LIFE = 2;
 const MIN_STAR_LIFE = 1;
-
 const MAX_STAR_SIZE = 30;
 const MIN_STAR_SIZE = 10;
-
 const MIN_STAR_TRAVEL_X = 50;
 const MIN_STAR_TRAVEL_Y = 50;
 
 const Star = class {
   constructor() {
     this.size = this.random(MAX_STAR_SIZE, MIN_STAR_SIZE);
-
     this.x = this.random(
       sparkle.offsetWidth * 0.75,
       sparkle.offsetWidth * 0.25
     );
-    this.y = sparkle.offsetHeight / 2 - this.size / 2;
 
+    this.y = sparkle.offsetHeight / 2 - this.size / 2;
     this.x_dir = this.randomMinus();
     this.y_dir = this.randomMinus();
-
-    this.x_max_travel =
-      this.x_dir === -1 ? this.x : sparkle.offsetWidth - this.x - this.size;
+    this.x_max_travel = this.x_dir === -1 ? this.x : sparkle.offsetWidth - this.x - this.size;
     this.y_max_travel = sparkle.offsetHeight / 2 - this.size;
-
     this.x_travel_dist = this.random(this.x_max_travel, MIN_STAR_TRAVEL_X);
     this.y_travel_dist = this.random(this.y_max_travel, MIN_STAR_TRAVEL_Y);
-
     this.x_end = this.x + this.x_travel_dist * this.x_dir;
     this.y_end = this.y + this.y_travel_dist * this.y_dir;
-
     this.life = this.random(MAX_STAR_LIFE, MIN_STAR_LIFE);
-
     this.star = document.createElement("div");
     this.star.classList.add("star");
-
     this.star.style.setProperty("--start-left", this.x + "px");
     this.star.style.setProperty("--start-top", this.y + "px");
-
     this.star.style.setProperty("--end-left", this.x_end + "px");
     this.star.style.setProperty("--end-top", this.y_end + "px");
-
     this.star.style.setProperty("--star-life", this.life + "s");
     this.star.style.setProperty("--star-life-num", this.life);
-
     this.star.style.setProperty("--star-size", this.size + "px");
   }
 
@@ -77,9 +61,7 @@ setInterval(() => {
   }
 
   current_star_count++;
-
   var newStar = new Star();
-
   newStar.draw();
 
   setTimeout(() => {
@@ -87,4 +69,5 @@ setInterval(() => {
 
     newStar.pop();
   }, newStar.life * 1000);
+  
 }, STAR_INTERVAL);
